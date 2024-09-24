cb97344283cc44000fd227eb43dacb18edd51ac0
gotax24 <124403304+gotax24@users.noreply.github.com>
2024-09-23 21:24:25 -0500

    agregando css a la pagina de  add.html

--- js/administrarLibro.js
+++ js/administrarLibro.js
@@ -33,6 +33,8 @@ const creacionInput = (id, placeholder, type) => {
   input.id = id;
   input.placeholder = placeholder;
   input.type = type;
+  input.classList.add('input')
+  input.classList.add('input-separacion')
 
   return input;
 };
@@ -43,6 +45,7 @@ const creacionButton = (text, id) => {
   let button = document.getElementById(id) || document.createElement("button");
   button.textContent = text;
   button.id = id;
+  button.classList.add('button-form')
 
   return button;
 };
@@ -162,3 +165,8 @@ document.getElementById("eliminarLibro").addEventListener("click", (e) => {
     dibujarTabla();
   });
 });
+
+function cerrarSesion() {
+  localStorage.removeItem("usuarioActivo");
+  window.location.href = "index.html";
+}
